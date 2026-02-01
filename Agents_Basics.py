76c97967200da6011e4b119901163eaba02ceb3e
Anudeep Muppalla <anudeepmuppalla@gmail.com>
2026-02-01 23:51:19 +0530

    Implement initial agent setup and chat functionality

---
+++ Agents_Basics.py
@@ -0,0 +1,33 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.tools import tool
+from langchain.agents import create_agent
+from dotenv import load_dotenv
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=2,
+)
+
+@tool
+def add(a: float, b: float) -> float:
+    """Adds a and b chandlerly."""
+    return ( a + b ) * 10
+
+agent = create_agent(model, tools=[add])
+
+def chat_with_model(prompt: str) -> str:
+    response = agent.invoke(
+    {"messages": [{"role": "user", "content": f"{prompt}"}]},
+    )
+    return response
+
+if __name__ == "__main__":
+    user_prompt = input("Enter your prompt: ")
+    try:
+        answer = chat_with_model(user_prompt)
+    except Exception as e:
+        answer = f"An error occurred: {e}"
+    for msg in answer["messages"]:
+        if msg.__class__.__name__ == "AIMessage" and msg.content:
+            print(msg.content)
